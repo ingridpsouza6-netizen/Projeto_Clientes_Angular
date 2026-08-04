688c946eaf5c00107aac7a8cfd499660b3c82e4f
Ingrid Souza <ingrid.p.souza6@aluno.senai.br>
2026-08-04 14:39:51 -0300

    criado formulário

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+//trouxe as ferramentas de formulário para usar no meu componente
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {}
+export class Cadastro {
+
+  formulario = new FormGroup({
+    //criei meu formulário 
+    nome: new FormControl(''),
+    email: new FormControl(''),
+    cpf: new FormControl(''),
+    dataNascimento: new FormControl(''),
+    uf: new FormControl(''),
+    municipio: new FormControl('')
+  });
+
+}
